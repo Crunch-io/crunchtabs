5a20d367b72695a1b8f0acd040e9fb37f3a851f4
Kamil Sedrowicz <kamilsedrowicz@Kamils-MacBook-Pro.local>
2017-03-21 15:02:43 +0100

    tabBook function is used to collect banner data
    a bug in the tabBook function has been fixed (incorrect values
    in the total column for categorical_array variables).
    the crtabs function is not used any more.

--- R/tabBooks.R
+++ R/tabBooks.R
@@ -15,10 +15,10 @@ tabBooks <- function(dataset, vars = names(dataset), banner = NULL, weight = NUL
 
   banner_map <- lapply(seq_along(banner), function(bx) sapply(banner[[bx]], function(bv) bv$alias))
   banner_flatten <- flattenBanner(banner)
-  var_names <- names(allVariables(dataset))
-  var_aliases <- aliases(allVariables(dataset))
-  var_types <- types(allVariables(dataset))
-  banner_var_aliases <- c("___total___", mtvars)
+  # var_names <- names(allVariables(dataset))
+  # var_aliases <- aliases(allVariables(dataset))
+  # var_types <- types(allVariables(dataset))
+  # banner_var_aliases <- c("___total___", mtvars)
 
   # NPR: can you add some inline comments so that I can follow along what you're doing?
   for (vi in seq_along(book)) {
@@ -26,16 +26,6 @@ tabBooks <- function(dataset, vars = names(dataset), banner = NULL, weight = NUL
     if (!(getAlias(crunch_cube)) %in% vars) next
 
     array_type <- type(dataset[[getAlias(crunch_cube)]]) == 'categorical_array'
-    if (getAlias(crunch_cube) != var_aliases[vi]) {
-      stop("getAlias(crunch_cube) != var_aliases[vi]")
-    }
-    if (getName(crunch_cube) != var_names[vi]) {
-      stop("getName(crunch_cube) != var_names[vi]")
-    }
-    if (type(dataset[[getAlias(crunch_cube)]]) != var_types[vi]) {
-      stop("type(dataset[[getAlias(crunch_cube)]]) != var_types[vi]")
-    }
-
     vnames <- if(array_type) paste(getName(crunch_cube), getSubNames(crunch_cube), sep = " - ") else getName(crunch_cube)
     valiases <- if(array_type) paste(getAlias(crunch_cube), getSubAliases(crunch_cube), sep = ".") else getAlias(crunch_cube)
 
@@ -47,68 +37,34 @@ tabBooks <- function(dataset, vars = names(dataset), banner = NULL, weight = NUL
                            , crosstabs = sapply(names(banner), function(x) list(), simplify = FALSE, USE.NAMES = TRUE)
       )
       class(tab_book_var) <- "CrossTabVar"
-
       tabs_data[[valiases[vai]]] <- tab_book_var
     }
 
-    crunch_cube_total <- crtabs(formula = paste("~", getAlias(crunch_cube)), data = dataset, weight = weight)
-    if (!is.null(weight)) {
-      crunch_cube_total_unweigheted <- crtabs(formula = paste("~", getAlias(crunch_cube)), data = dataset, weight = NULL)
-    }
-
     for (vbi in seq_along(book[[vi]])) {
-      if (array_type && vbi == 1) {
-        crunch_cube <- crunch_cube_total
-        margin <- 1
-      } else {
-        crunch_cube <- book[[vi]][[vbi]]
-        margin = if (array_type) c(1, 3) else 2
-      }
+      crunch_cube <- book[[vi]][[vbi]]
+      margin <- if (array_type) c(1, 3) else 2
 
       prop_banner_counts <- as.array(crunch_cube)
       prop_banner_proportions <- crunch::prop.table(crunch_cube, margin = margin)
-
-      prop_banner_sum_unweighted <- prop_banner_counts
-      if (!is.null(weight)) {
-        prop_banner_sum_unweighted <- if (array_type && vbi == 1)
-          as.array(crunch_cube_total_unweigheted)
-        else
-          as.array(crunch_cube, measure = 2)
-      }
+      prop_banner_counts_unweighted <- if (!is.null(weight)) bases(crunch_cube, margin = 0) else prop_banner_counts
 
       banner_var_alias <- if (vbi == 1) "___total___" else crunch_cube@.Data[[1]]$dimensions[[length(crunch_cube@.Data[[1]]$dimensions)]]$references$alias
 
-      if (banner_var_alias != banner_var_aliases[vbi]) {
-        stop("banner_var_alias != banner_var_aliases[vbi]")
-      }
-
-      # if (vbi == 1) {
-      #   if (array_type) {
-      #     prop_banner_counts <- array(prop_banner_counts, dim=c(dim(prop_banner_counts), 1), dimnames = c(dimnames(prop_banner_counts), list("total")))
-      #     prop_banner_proportions <- array(prop_banner_proportions, dim=c(dim(prop_banner_proportions), 1), dimnames = c(dimnames(prop_banner_proportions), list("total")))
-      #     prop_banner_sum_unweighted <- array(prop_banner_sum_unweighted, dim=c(dim(prop_banner_sum_unweighted), 1), dimnames = c(dimnames(prop_banner_sum_unweighted), list("total")))
-      #   }
-      # }
       for (ri in seq_along(valiases)) {
-        if (array_type && vbi > 1) {
-            counts_out = prop_banner_counts[ri, ,]
-            proportions_out = prop_banner_proportions[ri, ,]
-            prop_banner_sum_unweighted_out = prop_banner_sum_unweighted[ri, ,]
-        } else if (array_type && vbi == 1) {
-            counts_out = t(prop_banner_counts[ri,,drop = FALSE])
-            proportions_out = t(prop_banner_proportions[ri,,drop = FALSE])
-            prop_banner_sum_unweighted_out = t(prop_banner_sum_unweighted[ri,,drop = FALSE])
+        if (array_type) {
+          counts_out = prop_banner_counts[ri, ,]
+          proportions_out = prop_banner_proportions[ri, ,]
+          prop_banner_counts_unweighted_out = prop_banner_counts_unweighted[ri, ,]
         } else {
             counts_out = prop_banner_counts
             proportions_out = prop_banner_proportions
-            prop_banner_sum_unweighted_out = prop_banner_sum_unweighted
+            prop_banner_counts_unweighted_out = prop_banner_counts_unweighted
         }
 
         if (banner_var_alias != "___total___") {
           banner_var <- banner_flatten[[banner_var_alias]]
           counts_out <- bannerDataRecode(counts_out, banner_var)
-          prop_banner_sum_unweighted_out <- bannerDataRecode(prop_banner_sum_unweighted_out, banner_var)
-          # proportions_out <- bannerDataRecode(proportions_out, banner_var, divade = TRUE)
+          prop_banner_counts_unweighted_out <- bannerDataRecode(prop_banner_counts_unweighted_out, banner_var)
           proportions_out <- if (ncol(counts_out) == ncol(proportions_out)) {
             bannerDataRecode(proportions_out, banner_var)
           } else {
@@ -118,7 +74,7 @@ tabBooks <- function(dataset, vars = names(dataset), banner = NULL, weight = NUL
 
         totals_counts_out = colSums(counts_out)
         totals_proportions_out = colSums(proportions_out)
-        unweighted_n_out = colSums(prop_banner_sum_unweighted_out)
+        unweighted_n_out = colSums(prop_banner_counts_unweighted_out)
 
         if (vbi == 1) {
           colnames(counts_out) <- "Total"
