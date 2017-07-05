5078ee8d4001113942835b45b15808f327d90a72
Kamil Sedrowicz <kamil.sed@gmail.com>
2017-07-05 20:09:49 +0200

    prepare a test that loads a dataset (work in progress)

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,27 +2,20 @@ Sys.setlocale("LC_COLLATE", "C") ## What CRAN does; affects sort order
 set.seed(999) ## To ensure that tests that involve randomness are reproducible
 options(warn=1)
 
-## Load the crunch package test setup
-source(system.file("crunch-test.R", package="crunch"))
+unserializeJSON <- jsonlite::unserializeJSON
 
-fromJSON <- jsonlite::fromJSON
-unserializeJSON <- jsonlite::unserializeJSON
-unserializeJSON <- jsonlite::unserializeJSON
+with_mock_crunchtabs <- function (expr) {
+  env <- parent.frame()
+  with(temp.options(crunch.api="https://app.crunch.io/api/",
+                    httptest.mock.paths=c(".", system.file(package="crunchtabs"))), {
+                      with_mock_API({
+                        try(crunch:::warmSessionCache())
+                        eval(expr, envir=env)
+                      })
+                    })
+}
 
 fixtures_dir <- "fixtures"
-
-
-# with_mock_crunch <- function (expr) {
-#   env <- parent.frame()
-#   print(getwd())
-#   with(temp.options(crunch.api="https://app.crunch.io/api/"), {
-#                       with_mock_API({
-#                         try(crunch:::warmSessionCache())
-#                         eval(expr, envir=env)
-#                       })
-#                     })
-# }
-
 with_mock_tabs <- function(book_file, mt_file, expr) {
     with_mock(
         `crunch::tabBook`=function (...) {
@@ -35,8 +28,5 @@ with_mock_tabs <- function(book_file, mt_file, expr) {
     )
 }
 
-newDatasetFromFixture <- function (filename) {
-  m <- fromJSON(file.path(fixtures_dir, paste0(filename, ".json")), simplifyVector=FALSE)
-  return(suppressMessages(createWithMetadataAndFile(m, file.path(fixtures_dir, paste0(filename, ".csv")))))
-}
-
+## Load the crunch package test setup
+source(system.file("crunch-test.R", package="crunch"))
--- tests/testthat/test-tab-books.R
+++ tests/testthat/test-tab-books.R
@@ -1,9 +1,46 @@
-context('preparing banner data summary')
+context('Preparing banner data summary')
+test_that("We can read a Banner object from a file", {
+  txt <- readLines(con = file.path(fixtures_dir, "ds1_banner1.json"))
+  banner_data <- unserializeJSON(txt)
+  expect_is(banner_data, "Banner")
+})
 
+context('Loading a dataset from the crunch package')
+with_mock_crunch({
+  test_that("We can load a dataset from the crunch package", {
+    ds <- loadDataset("test ds")
+    expect_is(ds, "CrunchDataset")
+  })
+})
+
+context('Loading a dataset from the crunchtabas package')
+with_mock_crunchtabs({
+  test_that("We can load a dataset from the crunchtabas package", {
+    ds <- loadDataset("Example dataset")
+    expect_is(ds, "CrunchDataset")
+  })
+})
+
+context('Loading a dataset from the crunchtabas package with mock API')
 with_mock_API({
-  test_that("We can get a Banner object", {
-    txt <- readLines(con = file.path(fixtures_dir, "ds1_banner1.json"))
-    banner_data <- unserializeJSON(txt)
-    expect_is(banner_data, "Banner")
+  test_that("We can load a dataset from the crunchtabas package with mock API", {
+    ds <- loadDataset("Example dataset")
+    expect_is(ds, "CrunchDataset")
+  })
+})
+
+context('Loading a dataset from the crunchtabas package with mock API using URL')
+with_mock_API({
+  test_that("We can load a dataset from the crunchtabas package with mock API using URL", {
+    ds <- loadDataset("https://app.crunch.io/api/datasets/9955eddef2674cb895a4f91857965e9f/")
+    expect_is(ds, "CrunchDataset")
+  })
+})
+
+context('Loading a dataset from the crunchtabas package using URL')
+with_mock_crunchtabs({
+  test_that("We can load a dataset from the crunchtabas package using URL", {
+    ds <- loadDataset("https://app.crunch.io/api/datasets/9955eddef2674cb895a4f91857965e9f/")
+    expect_is(ds, "CrunchDataset")
   })
 })
