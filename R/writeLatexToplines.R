012aca79d6da26e6b2ad71e7ebd47dc3e41809d2
persephonet <persephone.tsebelis@yougov.com>
2017-11-03 12:28:28 +0000

    add 4 space indent

--- R/writeLatexToplines.R
+++ R/writeLatexToplines.R
@@ -13,92 +13,92 @@ writeLatex.Toplines <- function(data_summary, filename = NULL, proportions = TRU
     dc = c(3.2, 4.1), multirowheaderlines = FALSE,
     latex_adjust = 'c', clearpage = TRUE, grid_num_letters = TRUE, custom_numbering = NULL,
     round_percentages = FALSE) {
-
-  data_summary$results <- lapply(data_summary$results, function(x) {
-    x$data <- reformatResults(x, proportions = proportions, digits = digits, round_percentages = round_percentages)
-    x
-  })
-
-  headers <- lapply(seq_along(data_summary$results), function(i) {
-    toplineHeader(data_summary$results[[i]], page_width = page_width, row_label_width = row_label_width,
-                  num = if (!is.null(custom_numbering)) custom_numbering[i] else i)
-  })
-
-  footers <- lapply(data_summary$results, toplineFooter)
-  bodies <- lapply(data_summary$results, function(x) latexTable.body(x$data, dotfill = TRUE, autorownames = TRUE))
-
-  tables <- sapply(ltranspose(list(headers, bodies, footers)), function(x) paste(x, collapse = "\n"))
-
-  out <- c(tables, append_text)
-
-  if (!tableonly) {
-    latexHeadData <- latexHeadT(surveyhead = title, font_size = font_size, margin = margin, font = font, subhead = subtitle, landscape = landscape, graphicspath = graphicspath, logo = logo)
-    latexStartData <- latexStartT(table_of_contents = table_of_contents, sample_desc = sample_desc, field_period = field_period, moe = moe)
-    latexFootData <- latexFootT()
-    out <- c(latexHeadData, latexStartData, out, latexFootData)
-  }
-
-  if (!is.null(filename)) {
-    filename <- paste0(filename, ".tex")
-    cat(out, sep = "\n", file = filename)
-    if (pdf) {
-      pdflatex(filename, open, path.to.pdflatex = path.to.pdflatex)
+    
+    data_summary$results <- lapply(data_summary$results, function(x) {
+        x$data <- reformatResults(x, proportions = proportions, digits = digits, round_percentages = round_percentages)
+        x
+    })
+    
+    headers <- lapply(seq_along(data_summary$results), function(i) {
+        toplineHeader(data_summary$results[[i]], page_width = page_width, row_label_width = row_label_width,
+            num = if (!is.null(custom_numbering)) custom_numbering[i] else i)
+    })
+    
+    footers <- lapply(data_summary$results, toplineFooter)
+    bodies <- lapply(data_summary$results, function(x) latexTable.body(x$data, dotfill = TRUE, autorownames = TRUE))
+    
+    tables <- sapply(ltranspose(list(headers, bodies, footers)), function(x) paste(x, collapse = "\n"))
+    
+    out <- c(tables, append_text)
+    
+    if (!tableonly) {
+        latexHeadData <- latexHeadT(surveyhead = title, font_size = font_size, margin = margin, font = font, subhead = subtitle, landscape = landscape, graphicspath = graphicspath, logo = logo)
+        latexStartData <- latexStartT(table_of_contents = table_of_contents, sample_desc = sample_desc, field_period = field_period, moe = moe)
+        latexFootData <- latexFootT()
+        out <- c(latexHeadData, latexStartData, out, latexFootData)
     }
-  }
-
-  if (returndata) {
-    return(data_summary)
-  }
+    
+    if (!is.null(filename)) {
+        filename <- paste0(filename, ".tex")
+        cat(out, sep = "\n", file = filename)
+        if (pdf) {
+            pdflatex(filename, open, path.to.pdflatex = path.to.pdflatex)
+        }
+    }
+    
+    if (returndata) {
+        return(data_summary)
+    }
 }
 
 toplineHeader <- function(x, page_width = 6.5, num = NULL, row_label_width = 1.5, padding = 1, use_heuristic = TRUE) {
-  UseMethod("toplineHeader", x)
+    UseMethod("toplineHeader", x)
 }
 
 #' @export
 toplineHeader.default <- function(var_summary, page_width = 6.5, num = NULL, row_label_width = 1.5, padding = 1, use_heuristic = TRUE) {
-  tab_definition <- paste0("\\begin{tabular}{p{", page_width - padding, "in}}")
-  toplineTableDef(var_summary, page_width, num, tab_definition)
+    tab_definition <- paste0("\\begin{tabular}{p{", page_width - padding, "in}}")
+    toplineTableDef(var_summary, page_width, num, tab_definition)
 }
 
 #' @export
 toplineHeader.ToplineCategoricalArray <- function(var_summary, page_width = 6.5, num = NULL, row_label_width = 1.5, padding = 0.25, use_heuristic = TRUE) {
-  header_row <- "\n"
-  col_names <- getNames(var_summary)[[2]]
-  col_names_len <- length(col_names)
-  col_width <- paste(round(1/col_names_len, digits = 2), "\\mywidth", sep = "")
-  # use heuristic for scale questions
-  if (use_heuristic && col_names_len >= 10) {
-    which.split <- grep("^[0-9]+ - ", col_names)
-    if (length(which.split) == 2) {
-      labs <- escM(sub("^[0-9]+ - (.*)$", "\\1", col_names[which.split]))
-      mcwidth <- (max(which.split) - min(which.split) + 1)/2
-      midgaps <- 1 + ceiling(mcwidth)  ## in case it's an odd number
-      mcwidth <- floor(mcwidth)
-      midgaps <- midgaps - mcwidth
-      labs[1] <- paste("\\multicolumn{", mcwidth, "}{l}{", labs[1], "}", collapse = "")
-      labs[2] <- paste("\\multicolumn{", mcwidth, "}{r}{", labs[2], "}", collapse = "")
-      scalestart <- paste(rep(" &", min(which.split)), collapse = "")
-      scalemid <- paste(rep(" &", midgaps), collapse = "")
-      scaleend <- paste(rep(" &", length(col_names) - max(which.split)), collapse = "")
-      thisrow <- paste(scalestart, labs[1], scalemid, labs[2], scaleend, "\\\\")
-      header_row <- paste(header_row, thisrow, "\n")
-      col_names <- sub("^([0-9]+) - .*$", "\\1", col_names)
-      col_width <- paste(round((page_width - padding - 0.75 - row_label_width)/col_names_len - 0.11, 3), "in", sep = "")
+    header_row <- "\n"
+    col_names <- getNames(var_summary)[[2]]
+    col_names_len <- length(col_names)
+    col_width <- paste(round(1/col_names_len, digits = 2), "\\mywidth", sep = "")
+    # use heuristic for scale questions
+    if (use_heuristic && col_names_len >= 10) {
+        which.split <- grep("^[0-9]+ - ", col_names)
+        if (length(which.split) == 2) {
+            labs <- escM(sub("^[0-9]+ - (.*)$", "\\1", col_names[which.split]))
+            mcwidth <- (max(which.split) - min(which.split) + 1)/2
+            midgaps <- 1 + ceiling(mcwidth)  ## in case it's an odd number
+            mcwidth <- floor(mcwidth)
+            midgaps <- midgaps - mcwidth
+            labs[1] <- paste("\\multicolumn{", mcwidth, "}{l}{", labs[1], "}", collapse = "")
+            labs[2] <- paste("\\multicolumn{", mcwidth, "}{r}{", labs[2], "}", collapse = "")
+            scalestart <- paste(rep(" &", min(which.split)), collapse = "")
+            scalemid <- paste(rep(" &", midgaps), collapse = "")
+            scaleend <- paste(rep(" &", length(col_names) - max(which.split)), collapse = "")
+            thisrow <- paste(scalestart, labs[1], scalemid, labs[2], scaleend, "\\\\")
+            header_row <- paste(header_row, thisrow, "\n")
+            col_names <- sub("^([0-9]+) - .*$", "\\1", col_names)
+            col_width <- paste(round((page_width - padding - 0.75 - row_label_width)/col_names_len - 0.11, 3), "in", sep = "")
+        }
     }
-  }
-  header_row <- paste(header_row, "&", paste(escM(col_names), collapse = " & "), "\\\\\n")
-  col.header <- paste("B{\\centering}{", col_width, "}", sep = "")
-  col.header <- paste(rep(col.header, col_names_len), collapse = "")
-  tab_definition <- paste0("\\begin{tabular*}{", page_width - padding, "in}{@{\\extracolsep{\\fill}}B{\\raggedright}{", row_label_width,
-                           "in}", col.header, "}")
-
-  toplineTableDef(var_summary, page_width, num, tab_definition, header_row)
+    header_row <- paste(header_row, "&", paste(escM(col_names), collapse = " & "), "\\\\\n")
+    col.header <- paste("B{\\centering}{", col_width, "}", sep = "")
+    col.header <- paste(rep(col.header, col_names_len), collapse = "")
+    tab_definition <- paste0("\\begin{tabular*}{", page_width - padding, "in}{@{\\extracolsep{\\fill}}B{\\raggedright}{", row_label_width,
+        "in}", col.header, "}")
+    
+    toplineTableDef(var_summary, page_width, num, tab_definition, header_row)
 }
 
 toplineTableDef <- function(var_summary, page_width, num, tab_definition, header_row = '\n') {
-  filtertext <- getFilterText(var_summary)
-  paste("\\begin{table}[H]
+    filtertext <- getFilterText(var_summary)
+    paste("\\begin{table}[H]
         \\addcontentsline{lot}{table}{", escM(getName(var_summary)), "}
         \\colorbox{gray}{
         \\parbox{",
@@ -108,22 +108,22 @@ toplineTableDef <- function(var_summary, page_width, num, tab_definition, header
 }
 
 toplineFooter <- function(x) {
-  UseMethod("toplineFooter", x)
+    UseMethod("toplineFooter", x)
 }
 
 #' @export
 toplineFooter.default <- function(x) {
-  toplineFooterDef(is_grid = FALSE)
+    toplineFooterDef(is_grid = FALSE)
 }
 
 #' @export
 toplineFooter.ToplineCategoricalArray <- function(var_summary) {
-  toplineFooterDef(is_grid = TRUE)
-
+    toplineFooterDef(is_grid = TRUE)
+    
 }
 
 toplineFooterDef <- function(is_grid) {
-  paste0("\\end{", paste0("tabular", ifelse(is_grid, "*", "")), "}
+    paste0("\\end{", paste0("tabular", ifelse(is_grid, "*", "")), "}
          \\end{center}
          \\end{table}")
 }
@@ -133,7 +133,7 @@ latexFootT <- function() "\\end{hyphenrules} \n \\end{document}\n"
 
 
 latexHeadT <- function(surveyhead, font_size, margin, font, landscape=FALSE, subhead=NULL, graphicspath = NULL, logo = NULL){
-  paste("\\documentclass[", font_size, "pt", ifelse(landscape, ', landscape', ''), "]",
+    paste("\\documentclass[", font_size, "pt", ifelse(landscape, ', landscape', ''), "]",
         "{article}\n",
         "\\usepackage[pdftex]{graphicx}\n",
         if (!is.null(graphicspath)) paste0("\\graphicspath{ {", graphicspath,"/} }"),
@@ -161,8 +161,8 @@ latexHeadT <- function(surveyhead, font_size, margin, font, landscape=FALSE, sub
         "\\usepackage{marginnote}\n",
         "\\usepackage[",
         ifelse(!is.null(margin),
-               paste("top=", margin$t, "in, bottom=", margin$b, "in, left=", margin$l, "in, right=", margin$r, "in, includeheadfoot", sep=""),
-               "top=.6in,bottom=.6in,left=1in,right=1in,includeheadfoot"),
+            paste("top=", margin$t, "in, bottom=", margin$b, "in, left=", margin$l, "in, right=", margin$r, "in, includeheadfoot", sep=""),
+            "top=.6in,bottom=.6in,left=1in,right=1in,includeheadfoot"),
         "]{geometry}\n",
         "\\usepackage{array}\n",
         "\\setlength\\extrarowheight{2pt}\n",
