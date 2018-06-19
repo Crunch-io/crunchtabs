34b5e7f0a61c5f511791492f4c0942ab96987d79
persephonet <persephone.tsebelis@yougov.com>
2018-06-19 15:09:23 +0100

    updated fixture in gryphon and changes made to R script as a result

--- tests/testthat/fixtures/buildFixture.R
+++ tests/testthat/fixtures/buildFixture.R
@@ -11,6 +11,9 @@ url <- paste0("https://crunch-upload.uat.yougov.net/upload/crunchtabs_example/?s
 
 # Once the dataset has been uploaded, load it here:
 ds <- loadDataset(paste0("YouGov crunchtabs_example monkeys (starttime: from ", Sys.Date(), ")"))
+# Move it to the crunchtabs project so that you have it for the future
+proj <- projects()$crunchtabs
+datasets(proj) <- ds
 
 values(categories(ds$books2)) <- c(1:5, NA, NA, NA)
 subtotals(ds$books2) <- list(Subtotal(name = "3 or more", categories = 3:5, position = "bottom"))
@@ -41,26 +44,33 @@ is.na(categories(ds$art2))[ids(categories(ds$art2)) == 3] <- TRUE
 values(categories(ds$misc2)) <- c(1:3, NA, NA, NA, NA)
 ds$misc2_dk <- copyVariable(ds$misc2, name = "Importance Rank (with not ranked)")
 is.na(categories(ds$misc2_dk))[names(categories(ds$misc2_dk)) %in% c("don't know", "skipped")] <- FALSE
+mv(ds, ds["misc2_dk"], "/Miscellaneous")
+if (aliases(cd(ds, "/Miscellaneous"))[5] %in% "misc2_dk") setOrder(cd(ds, "/Miscellaneous"),
+    c(1:3, 5, 4))
 ds$art5_nonUniform <- copyVariable(ds$art5, name = "Art Talked About (Non Uniform Basis)")
 uniformBasis(ds$art5_nonUniform) <- FALSE
 notes(ds$art5_nonUniform) <- "Asked of those who like art form. Non uniform basis."
 mv(ds, ds["art5_nonUniform"], "/Art")
 
 values(categories(ds$political_attention)) <- c(0:10, NA, NA, NA)
-values(categories(ds$polAttentionpdl)) <- c(0:10, NA, NA, NA)
 subtotals(ds$profile_partyid) <- list(Subtotal(name = "Yes", categories = 1:5, after = -1))
 
 for (var in c("books1", "books3", "books7", "books8", "movies4", "tv2", "tv3", "tv5", 
-    "art2", "art4", "profile_ethnicity", "profile_gender", "profile_GOR", "profile_govregnew", 
-    "xprofile_gross_household", "profile_marital_stat", "profile_newstype", 
-    "profile_region", "profile_socialgrade_cie", "profile_work_industry", 
-    "profile_education_level_recode", "xprofile_religion", "profile_partyid", 
-    "profile_past_vote_2005", "profile_past_vote_2010", "pastvote_2015_by_region", 
-    "political_attention", "pastvote_2015_recode", "polAttentionpdl", "exit_status", 
-    "disposition", "has_profanity", "has_media_error", "has_test")) {
+    "art2", "art4", "age", "age4", "profile_gender", "age4_gender", "profile_socialgrade_cie", 
+    "socialgrade_weight", "profile_ethnicity", "profile_GOR", "profile_govregnew", 
+    "profile_region", "profile_education_level_recode", "xprofile_gross_household", 
+    "profile_bpcnews", "profile_newstype", "profile_marital_stat", "profile_work_industry", 
+    "xprofile_religion", "profile_partyid", "pastvote_2015_recode", "pastvote_2015_by_region", 
+    "profile_past_vote_2010", "profile_past_vote_2005",    "exit_status", "disposition", 
+    "has_profanity", "has_media_error", "has_test")) {
     values(categories(ds[[var]])) <- NA
 }
-
+ 
+weight(ds) <- makeWeight(age4_gender ~ c(7.6,12.1,12.9,16,7.2,12.2,13.2,18.8),
+    socialgrade_weight ~ c(25.9,29.1,20.7,16.2,8.1),
+    profile_bpcnews ~ c(16,22,16,4,9.5,12.5,20),
+    profile_GOR ~ c(4.2,11.2,8.5,7.3,8.8,9.3,12.4,13.6,8.5,4.9,8.4,2.9,0),
+    name = "weight", alias = "weight")
 
 
 
