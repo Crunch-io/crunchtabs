f8443d49093e3dc22a16fbf4d0d5169ed71d8cc1
Brandon Bertelsen <brandon@bertelsen.ca>
2020-04-13 17:06:58 -0500

    Refs #67

--- R/tex-table.R
+++ R/tex-table.R
@@ -108,7 +108,7 @@ latexTableBody <- function(df, theme) {
   }
 
   # Escape everything
-  data <- lapply(data, function(dt) dfapply(dt, texEscape, Names=TRUE))
+  data <- lapply(data, function(dt) dfapply(dt, texEscape, Names = TRUE))
 
   # Apply additional styles to whole table sections
   # TODO: add tests for this
@@ -117,7 +117,7 @@ latexTableBody <- function(df, theme) {
       data[[nm]],
       applyLatexStyle,
       theme[[paste0("format_", nm)]],
-      Names=TRUE
+      Names = TRUE
     )
   }
 
@@ -142,7 +142,7 @@ latexTableBody <- function(df, theme) {
 
     # Also, IMPORTANT: cat arrays get displayed transposed. First, paste
     # by column to generate the table rows
-    rows <- lapply(data$body, paste, collapse=" & ")
+    rows <- lapply(data$body, paste, collapse = " & ")
     # Add to each row an empty cell (idk why), the row name (from the df
     # column names), and a newline, and collapse them into a single string
     return(paste(
@@ -151,7 +151,7 @@ latexTableBody <- function(df, theme) {
       "&",
       rows,
       newline,
-      collapse="\n"
+      collapse = "\n"
     ))
   }
 
@@ -363,17 +363,48 @@ tableHeader.ToplineCategoricalArray <- function(var, theme) {
                          paste(c("", "", texEscape(col_names), ""), collapse = " & "),
                          "\\\\\n")
   }
-  col.header <- paste0("B{\\centering}{", col_width, "}")
-  col.header <- paste(rep(col.header, col_names_len + 1), collapse = "")
-  tab_definition <- paste0(
-    "\\begin{", ifelse(var$longtable, "longtable", "tabular"), "}",
-    "{",
-    "@{\\extracolsep{\\fill}}",
-    "p{0.1in}",
-    "B{\\raggedright}{", theme$format_label_column$col_width, "in}",
-    col.header,
-    "}"
-  )
+
+
+
+  # Issue # 67: Add smart widths
+  if (is.na(theme$format_label_column$col_width)) {
+
+    col_width_perc = c(.18, .18, .17, 0.16, 0.15, 0.14, 0.12, 0.10)[col_names_len]
+
+    col_width <- paste(round(col_width_perc, digits = 2), "\\mywidth", sep = "")
+    first_col_width <- rev(seq(1.5, 4.5, by = 0.5))[col_names_len]
+
+    col.header <- paste0("B{\\centering}{", col_width, "}")
+    col.header <- paste0(
+      paste(rep(col.header, col_names_len + 1), collapse = "")
+    )
+
+    tab_definition <- paste0(
+      "\\begin{", ifelse(var$longtable, "longtable", "tabular"), "}",
+      "{",
+      "@{\\extracolsep{\\fill}}",
+      "p{0.1in}",
+      "B{\\raggedright}{", first_col_width , "in}",
+      col.header,
+      "}"
+    )
+
+  } else {
+
+    col_width <- paste(round(1/col_names_len, digits = 2), "\\mywidth", sep = "")
+    col.header <- paste0("B{\\centering}{", col_width, "}")
+    col.header <- paste(rep(col.header, col_names_len + 1), collapse = "")
+
+    tab_definition <- paste0(
+      "\\begin{", ifelse(var$longtable, "longtable", "tabular"), "}",
+      "{",
+      "@{\\extracolsep{\\fill}}",
+      "p{0.1in}",
+      "B{\\raggedright}{", theme$format_label_column$col_width, "in}",
+      col.header,
+      "}"
+    )
+  }
 
   toplineTableDef(
     var,
