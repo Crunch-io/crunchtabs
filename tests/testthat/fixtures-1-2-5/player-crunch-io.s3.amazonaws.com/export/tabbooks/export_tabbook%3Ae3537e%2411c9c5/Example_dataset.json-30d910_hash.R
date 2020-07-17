00e41576042a5616425d19098edf01d0e46c1d01
Greg Freedman Ellis <greg@crunch.io>
2020-07-17 14:43:05 -0500

    two more snuck in

--- tests/testthat/fixtures-1-2-5/player-crunch-io.s3.amazonaws.com/export/tabbooks/export_tabbook%3Ae3537e%2411c9c5/Example_dataset.json-30d910_hash.R
+++
@@ -1,1 +0,0 @@
-list(query_string = "Signature=obOl3dWycXT3x3hTytuB6SAOEtA%3D&Expires=1595017345&AWSAccessKeyId=AKIA5IDTGVXERUPU3ZRY")
--- tests/testthat/fixtures-1-2-5/player-crunch-io.s3.amazonaws.com/export/tabbooks/export_tabbook%3Ae3537e%2411c9c5/Example_dataset.json-feea5e_hash.R
+++
@@ -1,1 +0,0 @@
-list(query_string = "Signature=vCJAMHNKQGDWQcsYaUBMe2NqxeE%3D&Expires=1595017354&AWSAccessKeyId=AKIA5IDTGVXE3OOQLO7I")
