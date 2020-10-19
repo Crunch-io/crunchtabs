b7bf3fe9e2cdd9067e479a6cf0783610828b3a50
Brandon Bertelsen <brandon@bertelsen.ca>
2020-10-19 15:10:56 -0500

    Adding more tests

--- dev-misc/recontact_toplines.R
+++ dev-misc/recontact_toplines.R
@@ -4,6 +4,7 @@ library(crunchtabs)
 login()
 ds <- loadDataset("Example dataset")
 
+# debugonce(recontact_toplines)
 ct <- recontact_toplines(
   ds,
   questions = c("q1", "country"),
@@ -20,20 +21,22 @@ theme <- themeNew(default_theme = themeDefaultLatex(), latex_flip_grids = TRUE)
 writeLatex(ct, pdf = TRUE, open = TRUE, theme = theme)
 # Data setup -----
 
-# library(crunch)
-# login()
-# with_consent(deleteDataset("Example dataset"))
-# ds <- newExampleDataset()
-# ds <- loadDataset("Example dataset")
-#
-# ds$weight1 <- makeWeight(ds$q1 ~ c(0.3,0.3,0.4,0), name = 'weight1')
-# ds$weight2 <- makeWeight(ds$q1 ~ c(0.4,0.4,0.1,0.1), name = 'weight2')
-# weight(ds) <- ds$weight1
-#
-# ds$q1_pre <- copyVariable(ds$q1, deep = TRUE, name = "Pet name pre")
-# ds$q1_post <- copyVariable(ds$q1, deep = TRUE, name = "Pet name post")
-# ds$country_pre <- copyVariable(ds$country, deep = TRUE, name = "Country pre")
-# ds$country_post <- copyVariable(ds$country, deep = TRUE, name = "Country post")
-#
-# questions = c("q1", "country")
-# suffixes = c("_pre", "_post")
+library(crunch)
+login()
+with_consent(deleteDataset("Example dataset"))
+ds <- newExampleDataset()
+ds <- loadDataset("Example dataset")
+
+ds$weight1 <- makeWeight(ds$q1 ~ c(0.3,0.3,0.4,0), name = 'weight1')
+ds$weight2 <- makeWeight(ds$q1 ~ c(0.4,0.4,0.1,0.1), name = 'weight2')
+weight(ds) <- ds$weight1
+weight(ds) <- ds$weight2
+weight(ds) <- ds$weight1
+
+ds$q1_pre <- copyVariable(ds$q1, deep = TRUE, name = "Pet name pre")
+ds$q1_post <- copyVariable(ds$q1, deep = TRUE, name = "Pet name post")
+ds$country_pre <- copyVariable(ds$country, deep = TRUE, name = "Country pre")
+ds$country_post <- copyVariable(ds$country, deep = TRUE, name = "Country post")
+
+questions = c("q1", "country")
+suffixes = c("_pre", "_post")
---
+++ tests/testthat/test-recontact_toplines.R
@@ -0,0 +1,89 @@
+context('recontact_toplines')
+
+test_that("Stops if not a dataset", {
+  expect_error(
+    recontact_toplines(
+      "String is not a dataset",
+      questions = "q1",
+      suffixes = c("_pre", "_post"),
+      labels = c("Before", "After"),
+      weights = NULL
+    ),
+    "is.dataset(dataset) is not TRUE",
+    fixed = TRUE
+  )
+})
+
+test_that("Stops if questions not character", {
+  expect_error(
+    with_mock(
+      recontact_toplines(
+    "mocked",
+    questions = 1L, # not character
+    suffixes = c("_pre", "_post"),
+    labels = c("Before", "After"),
+    weights = NULL
+      ),
+    `crunch::is.dataset` = function(...) TRUE,
+  ),
+  "is.character(questions) is not TRUE", fixed = TRUE)
+})
+test_that("Stops if suffixes not character", {
+  expect_error(
+    with_mock(
+      recontact_toplines(
+        "mocked",
+        questions = "character",
+        suffixes = 1L, # not character
+        labels = c("Before", "After"),
+        weights = NULL
+      ),
+      `crunch::is.dataset` = function(...) TRUE
+    ),
+    "is.character(suffixes) is not TRUE", fixed = TRUE)
+})
+
+test_that("Stops if labels not character", {
+  expect_error(
+    with_mock(
+      recontact_toplines(
+        "String is not a dataset",
+        questions = "text to pass check",
+        suffixes = "text to pass check",
+        labels = 1L,
+        weights = NULL
+      ),
+      `crunch::is.dataset` = function(...) TRUE
+    ),
+    "is.character(labels) is not TRUE", fixed = TRUE)
+})
+
+test_that("End to end", {
+  ds <- list()
+
+  r <- with_mock(
+    recontact_toplines(
+      ds,
+      questions = c("q1", "country"),
+      suffixes = c("_pre", "_post"),
+      labels = c("Pre", "Post"),
+      weights = c("weight1", "weight2")
+    ),
+    `crunch::is.dataset` = function(...) TRUE,
+    `crunch::weight` = function(...) "weight1",
+    `crunch::alias` = function(...) "weight1",
+    `crunchtabs::crosstabs` = function(...) {
+      return(readRDS('fixtures/recontact_toplines.rds'))
+    }
+  )
+
+  expect_named(r$results, c("q1", "country"))
+  expect_is(
+    r$results$q1,
+    c("ToplineCategoricalArray", "ToplineVar", "CrossTabVar")
+  )
+  expect_is(
+    r$results$country,
+    c("ToplineCategoricalArray", "ToplineVar", "CrossTabVar")
+  )
+})
